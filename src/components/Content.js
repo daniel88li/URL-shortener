e2a12ddfc3de43724730f6dda3e83156d1c40293
Daniel Li <daniel88_li@hotmail.com>
2021-03-18 19:34:10 -0400

    Added mobile view of the statistic section

--- src/components/Content.js
+++ src/components/Content.js
@@ -38,35 +38,42 @@ function Content() {
       <Shortener />
 
       <div className="stat-box">
-        <h4>Advanced Statistics</h4>
+        <h3>Advanced Statistics</h3>
         <p>
           Track how your links are performing across the web with our advanced
           statistics dashboard.
         </p>
-      </div>
 
-      <div>
-        <h4>Brand Recognition</h4>
-        <p>
-          Boost your brand recognition with each click. Generic links don’t mean
-          a thing. Branded links help instil confidence in your content.
-        </p>
-      </div>
+        <div className="stat-1">
+          <div className="stat-icons"></div>
+          <h4>Brand Recognition</h4>
+          <p>
+            Boost your brand recognition with each click. Generic links don’t
+            mean a thing. Branded links help instil confidence in your content.
+          </p>
+        </div>
 
-      <div>
-        <h4>Detailed Records</h4>
-        <p>
-          Gain insights into who is clicking your links. Knowing when and where
-          people engage with your content helps inform better decisions.
-        </p>
-      </div>
+        <div className="separator"></div>
 
-      <div>
-        <h4>Fully Customizable</h4>
-        <p>
-          Improve brand awareness and content discoverability through
-          customizable links, supercharging audience engagement.
-        </p>
+        <div className="stat-2">
+          <div className="stat-icons"></div>
+          <h4>Detailed Records</h4>
+          <p>
+            Gain insights into who is clicking your links. Knowing when and
+            where people engage with your content helps inform better decisions.
+          </p>
+        </div>
+
+        <div className="separator"></div>
+
+        <div className="stat-3">
+          <div className="stat-icons"></div>
+          <h4>Fully Customizable</h4>
+          <p>
+            Improve brand awareness and content discoverability through
+            customizable links, supercharging audience engagement.
+          </p>
+        </div>
       </div>
 
       <div className="boost-box">
